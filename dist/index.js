69c7404ce627424fe72d2261c9a83aefc50ce11d
Tugay Serinken <tugay.serinken@hotmail.de>
2019-12-12 22:18:14 +0700

    fix

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,6 @@
-'use strict';
+import express from 'express';
+import proxy from 'express-http-proxy';
 
-var _express = require('express');
-
-var _express2 = _interopRequireDefault(_express);
-
-var _expressHttpProxy = require('express-http-proxy');
-
-var _expressHttpProxy2 = _interopRequireDefault(_expressHttpProxy);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var app = (0, _express2.default)();
-app.use('/', (0, _expressHttpProxy2.default)('http://10.10.120.10:1337/'));
+const app = express();
+app.use('/', proxy('http://10.10.120.10:1337/'));
 app.listen(4000, '0.0.0.0');
