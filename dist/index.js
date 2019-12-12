486ad1032a024504718c7aecc9adabdb3da58859
Tugay Serinken <tugay.serinken@hotmail.de>
2019-12-12 22:22:38 +0700

    remove es6 syntax

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import proxy from 'express-http-proxy';
+var express = require('express');
+var proxy = require('express-http-proxy');
 
-const app = express();
+var app = express();
 app.use('/', proxy('http://10.10.120.10:1337/'));
 app.listen(4000, '0.0.0.0');
